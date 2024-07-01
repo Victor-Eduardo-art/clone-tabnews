13e26efb1087498f674d54388154e2623da011ec
Victor Eduardo art <vcladara@gmail.com>
2024-06-30 22:05:26 -0300

    adciona a pasta pages e a pagina inicial

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Barbara... mesmo que o trabalho esteja difícil não se esqueça de aproveita os momentos tá?</h1>
+        </>
+    );
+}
