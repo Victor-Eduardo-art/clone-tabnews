54b36f6bad63b133b20caf6f2a9aa4e1fff52642
Victor Eduardo art <vcladara@gmail.com>
2024-07-01 19:41:23 -0300

    adicionei o SpeedInsights da Vercel

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function Home() {
     return (
         <>
+            <SpeedInsights></SpeedInsights>
             <h1>Barbara... mesmo que o trabalho esteja difícil não se esqueça de aproveita os momentos tá?</h1>
 
             <h2>Adicionar novas coisas aqui</h2>
